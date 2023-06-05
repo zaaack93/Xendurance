6fa70be0a2c445821a6d1798cde2da90c385790f
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-06-05 20:04:57 +0100

    add font files

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,6 @@ const mix = require("laravel-mix");
 mix
     .js("./src/js/main.js", "assets")
     .sass("./src/scss/main.scss", "assets")
+    .copy("./src/scss/fonts/*.otf", "assets")
+    .options({ processCssUrls: false })
     .setPublicPath("");
