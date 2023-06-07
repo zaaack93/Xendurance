4ab5260cb181c5aafc8026591f8a45bc27456083
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-06-07 21:47:25 +0100

    fix the compare att price for the product info add update the price when variant is changed

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,22 @@
             }
             });
         });
+
+        document.addEventListener('variant:change', function(event) {
+            let variant = event.detail.variant; // Gives you access to the whole variant details
+            let product = event.detail.product; // Gives you access to the whole product that the variant belongs to
+            let form = event.target; // The form that triggers the change
+            if(variant.available){
+                let newItem = `&nbsp;&nbsp;|&nbsp;<span  data-smartrr-product-id="${ product.id }" data-smartrr-price-style="overwrite-compare" data-use-quantity="true"><span data-smartrr-compare-price></span><span data-smartrr-regular-price></span><span smartrr-top-price data-smartrr-subscribe-price></span></span>`;
+                form.querySelector('button[type="submit"]').querySelector('div').innerHTML+=newItem
+
+                //this button reload the sell price for button add to cart
+                const radioBtns = document.querySelectorAll('input[name="purchase_option"]');
+                radioBtns.forEach(radioBtn => {
+                    const changeEvent = new Event('change');
+                    radioBtn.dispatchEvent(changeEvent);
+                });
+            }
+        });
     }
 })();
