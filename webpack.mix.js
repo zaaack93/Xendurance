6aca230f6d89359d34f713bdf7874df08b6843dd
bouzangad zakaria <bouzangadzakaria@gmail.com>
2025-04-09 23:36:49 +0100

    Add WOFF and WOFF2 font files for Clan Pro; update font-face declarations and webpack configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,7 @@ mix
     .js("./src/js/main.js", "assets")
     .sass("./src/scss/main.scss", "assets")
     .copy("./src/scss/fonts/*.otf", "assets")
+    .copy("./src/scss/fonts/*.woff", "assets")
+    .copy("./src/scss/fonts/*.woff2", "assets")
     .options({ processCssUrls: false })
     .setPublicPath("");
